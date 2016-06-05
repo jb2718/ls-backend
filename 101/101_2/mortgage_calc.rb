e3fc33783016f4ff89eef955fa5cafb12053e8cb
jb2718 <jb2718@users.noreply.github.com>
2016-06-05 08:03:20 -0400

    refactored program - by adding validations, running code through rubocop, and improving display of results

--- 101/101_2/mortgage_calc.rb
+++ 101/101_2/mortgage_calc.rb
@@ -1,53 +1,113 @@
-# You'll need three pieces of information:
+def prompt(message)
+  puts "=> #{message}"
+end
 
-# the loan amount
-# the Annual Percentage Rate (APR)
-# the loan duration
-# From the above, you'll need to calculate the following two things:
+def valid_dollar_amt?(number)
+  regex = /\A(\d*\.{0,1}\d{0,2})\z/
 
-# monthly interest rate
-# loan duration in months
+  if !number.include?(",") || !regex.match(number).nil?
+    true
+  else
+    false
+  end
+end
 
-# P = L[c(1 + c)n]/[(1 + c)n - 1]
+def valid_apr?(value)
+  regex = /\A(\d{1,3}\.{0,1}\d*)\z/
+  if !regex.match(value).nil?
+    value = regex.match(value)[0].to_f
+    if value >= 0 && value <= 100
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
 
-
-
-#===========PseudoCode===========
-# Display welcome msg
-# Ask user for loan amount
-# Ask user for APR
-# Ask user for loan duration
-# Calculate monthly payment
-# # ====================================
+def valid_years?(value)
+  regex = /\A(\d{1,2})\z/
+  if !regex.match(value).nil?
+    value = regex.match(value)[0].to_i
+    if value > 0 && value <= 40
+      true
+    else
+      false
+    end
+  else
+    false
+  end
+end
 
 puts "===================================="
 puts "      Mortgage Caculator App"
 puts "===================================="
 
-puts "Welcome to the Mortgage Caculator!"
+prompt "Welcome to the Mortgage Caculator!"
 
-puts "How much money do you need to borrow to purchase the house?"
-loan = gets.chomp.to_f
+loop do # main loop
+  loan = ""
+  loop do
+    prompt "How much money do you need to borrow to purchase the house?"
+    prompt "(Please do not enter commas ',''):"
+    loan = gets.chomp
 
-puts "How much will you be charged each year to borrow this money?"
-puts "(Please enter the APR as a number between 0 and 100): "
-apr = gets.chomp
-monthly_int = apr.to_f/(100 *12)
+    if valid_dollar_amt?(loan)
+      break
+    else
+      prompt "Invalid dollar amount."
+      prompt "Make sure to have no more than 2 digits after the decimal."
+      prompt "Please try again!"
+    end
+  end
 
-puts "How many years do you need to pay the loan back?"
-term_years = gets.chomp.to_f
-term_months = term_years * 12
+  apr = ""
+  loop do
+    prompt "How much will you be charged each year to borrow this money?"
+    prompt "(Please enter the APR as a number between 0 and 100): "
+    apr = gets.chomp
+    if valid_apr?(apr)
+      break
+    else
+      prompt "That's not a valid input.  Please try again!"
+    end
+  end
 
+  term_years = ""
+  loop do
+    prompt "How many years do you need to pay the loan back?"
+    term_years = gets.chomp
+    if valid_years?(term_years)
+      break
+    else
+      prompt "That's not a valid input."
+      prompt "Make sure to enter an integer between 1 and 40."
+      prompt "Please try again!"
+    end
+  end
 
-# P = L[c(1 + c)n]/[(1 + c)n - 1]
-monthy_payment_ratio_num = monthly_int * (1 + monthly_int)**term_months 
-monthy_payment_ratio_denom = (1 + monthly_int)**term_months - 1
-monthly_payment = loan * ( monthy_payment_ratio_num / monthy_payment_ratio_denom)
+  prompt "Calculating monthly payment..."
 
-response = <<-RESP
-In order to buy a property that costs $#{loan}, 
-you will need to pay #{monthly_payment} per month
-for #{term_months} months (that's #{term_years} years)
+  # P = L[c(1 + c)n]/[(1 + c)n - 1]
+  monthly_int = apr.to_f / (100 * 12)
+  term_months = term_years.to_i * 12
 
-RESP
-puts response
+  monthy_payment_ratio_num = monthly_int * (1 + monthly_int)**term_months
+  monthy_payment_ratio_denom = (1 + monthly_int)**term_months - 1
+  monthly_payment = loan.to_f * (monthy_payment_ratio_num / monthy_payment_ratio_denom)
+
+  response = <<-RESP
+  In order to buy a property that costs $#{loan},
+  you will need to pay $#{sprintf('%#.2f', monthly_payment.round(2))} per month
+  for #{term_months} months (that's #{term_years} years)
+
+  RESP
+  prompt response
+
+  prompt "Do you want to do another calculation? (Y to calculate again): "
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+end # end main loop
+
+prompt "Thanks for using Mortgage Calculator!  Goodbye!"
