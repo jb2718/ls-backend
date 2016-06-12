e4799aa654513e39657ce2a269227c5788cf1ec4
jb2718-SE <jbattle2718@gmail.com>
2016-06-12 08:09:14 -0400

    cleared ruboocop infractions

--- 101/101_2/rock_paper_scissors/main.rb
+++ 101/101_2/rock_paper_scissors/main.rb
@@ -1,46 +1,58 @@
 # main.rb rock paper scissors main file
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = %w(rock paper scissors)
+
+def test_method
+  prompt('test method')
+end
+
+# test_method - cannot invoke this method bf defining prompt method
+
 def prompt(message)
-	puts "=> #{message}"
+  puts "=> #{message}"
+end
+
+def won?(first, second)
+  win = [%w(rock scissors), %w(paper rock), %w(scissors paper)]
+  check = []
+  check << first << second
+  return true if win.include?(check)
+
+  false
 end
 
 def display_results(player, computer)
-	if (player == 'rock' && computer == 'scissors') ||
-		(player == 'paper' && computer == 'rock') ||
-		(player == 'scissors' && computer == 'paper')
-		prompt "You won!"
-	elsif (player == 'rock' && computer == 'paper') ||
-		(player == 'paper' && computer == 'scissors') ||
-		(player == 'scissors' && computer == 'rock')
-		prompt "Computer won!"
-	else
-		prompt "It's a tie!"
-	end
+  if won?(player, computer)
+    prompt "You won!"
+  elsif won?(computer, player)
+    prompt "Computer won!"
+  else
+    prompt "It's a tie!"
+  end
 end
 
-loop do 
-	choice = ''
+loop do
+  choice = ''
 
-	loop do
-		prompt "Choose one: #{VALID_CHOICES.join(', ')}"
-		choice = gets.chomp
+  loop do
+    prompt "Choose one: #{VALID_CHOICES.join(', ')}"
+    choice = gets.chomp
 
-		if VALID_CHOICES.include?(choice)
-			break
-		else
-			prompt "That is not a valid choice."
-		end	
-	end
+    if VALID_CHOICES.include?(choice)
+      break
+    else
+      prompt "That is not a valid choice."
+    end
+  end
 
-	computer_choice = ['rock', 'paper', 'scissors'].sample
+  computer_choice = %w(rock paper scissors).sample
 
-	prompt "You chose: #{choice}; Computer chose #{computer_choice}"
-	
-	display_results(choice, computer_choice)
+  prompt "You chose: #{choice}; Computer chose #{computer_choice}"
 
-	prompt "Would you like to play again? (Yes or No)"
-	play_again = gets.chomp
-	break unless play_again.downcase.start_with?('y')
+  display_results(choice, computer_choice)
+
+  prompt "Would you like to play again? (Yes or No)"
+  play_again = gets.chomp
+  break unless play_again.downcase.start_with?('y')
 end
 
 prompt "Thank you for playing.  Goodbye!"
