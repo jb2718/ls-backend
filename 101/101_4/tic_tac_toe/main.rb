5d1d4aa5fd3f024a9cae98a340d50f96665a8227
jb2718-SE <jbattle2718@gmail.com>
2016-06-22 04:46:24 -0400

    finished running code through rubocop and refactoring

--- 101/101_4/tic_tac_toe/main.rb
+++ 101/101_4/tic_tac_toe/main.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
+PLAYER_MARK = 'X'.freeze
+COMPUTER_MARK = 'O'.freeze
+EMPTY_SQUARE_MARK = '*'.freeze
+WINNING_COMBOS = [
+  [1, 2, 3], [4, 5, 6], [7, 8, 9],
+  [1, 4, 7], [2, 5, 8], [3, 6, 9],
+  [1, 5, 9], [3, 5, 7]
+].freeze
+
+# rubocop:disable Performance/Casecmp
 def play_again?(response)
   if response.downcase == 'y' || response.downcase == 'yes'
     return 1
@@ -5,8 +17,9 @@ def play_again?(response)
     return 0
   else
     return -1
-  end   
+  end
 end
+# rubocop:enable Performance/Casecmp
 
 def valid_move?(move, board)
   board.include?(move)
@@ -16,100 +29,100 @@ def prompt(message)
   puts "=> #{message}"
 end
 
-def winner?(moves, board)
-  winning_combos = [
-    [1,2,3], [4,5,6], [7,8,9],
-    [1,4,7], [2,5,8], [3,6,9],
-    [1,5,9], [3,5,7]
-  ]
-  winning_combos.each do |combo|
-    three_in_a_row = 0
+def winner?(moves)
+  WINNING_COMBOS.each do |combo|
+    marks_in_a_row = 0
     combo.each do |number|
       break unless moves.include?(number)
-      three_in_a_row += 1
+      marks_in_a_row += 1
     end
-    return true if three_in_a_row == 3
+    return true if marks_in_a_row == 3
   end
   false
 end
 
-def tie?(board)
-  board.empty?
+def tie?(brd)
+  brd[:empty_squares].empty?
 end
 
-def display_board(x_list, o_list)
-  current_board = ['1','2','3','4','5','6','7','8','9']
-  x_list.each{ |square| current_board[square - 1] = 'X' }
-  o_list.each{ |square| current_board[square - 1] = 'O' }
-  row1 = current_board[0].ljust(6) + current_board[1].ljust(6) + current_board[2]
-  row2 = current_board[3].ljust(6) + current_board[4].ljust(6) + current_board[5]
-  row3 = current_board[6].ljust(6) + current_board[7].ljust(6) + current_board[8]
+# rubocop:disable Metrics/AbcSize
+def display_board(brd)
+  system "clear"
+  puts "Welcome to Tic-Tac-Toe"
+  prompt "You are #{PLAYER_MARK}'s."
+  prompt "Computer is #{COMPUTER_MARK}'s.\n\n"
+  squares = []
+  9.times { squares << EMPTY_SQUARE_MARK }
+  brd[:player_moves].each { |square| squares[square - 1] = PLAYER_MARK }
+  brd[:computer_moves].each { |square| squares[square - 1] = COMPUTER_MARK }
+  row1 = squares[0].ljust(6) + squares[1].ljust(6) + squares[2]
+  row2 = squares[3].ljust(6) + squares[4].ljust(6) + squares[5]
+  row3 = squares[6].ljust(6) + squares[7].ljust(6) + squares[8]
+  puts row1.center(30)
+  puts row2.center(30)
+  puts row3.center(30)
+  puts "\nAvailable squares: " + brd[:empty_squares].to_s
+end
+# rubocop enable Metrics/AbcSize
 
-  puts row1.center(40)
-  puts row2.center(40)
-  puts row3.center(40)
+def new_board
+  {
+    empty_squares: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+    player_moves: [],
+    computer_moves: []
+  }
 end
 
-puts "Welcome to Tic-Tac-Toe!"
+def player_turn(brd)
+  loop do
+    prompt "On which square will you put your '#{PLAYER_MARK}'?"
+    move = gets.chomp.to_i
+    if valid_move?(move, brd[:empty_squares])
+      brd[:player_moves] << move
+      brd[:empty_squares].delete(move)
+      break
+    else
+      prompt "That choice is not valid. Please try again!"
+    end
+  end
+end
 
-play_again = 'no'
+def computer_turn(brd)
+  brd[:computer_moves] << brd[:empty_squares].sample
+  brd[:empty_squares].delete(brd[:computer_moves].last)
+end
 
 loop do
-  board_options = [1,2,3,4,5,6,7,8,9]
-  player_moves = []
-  computer_moves = []
-  prompt "Here's a new board"
-  display_board(player_moves, computer_moves)
-  p board_options
-
+  board = new_board
+  display_board(board)
 
   loop do
-    prompt "You are X's." 
-    loop do
-      prompt "Choose which square to put your 'X'"
-      move = gets.chomp.to_i
-      if valid_move?(move,board_options)
-        player_moves << move
-        board_options.delete(move)
-        break
-      else
-        prompt "That choice is not valid. Please try again!"
-      end
-    end
+    player_turn(board)
+    display_board(board)
+    break if tie?(board) || winner?(board[:player_moves])
 
-    prompt "Here's the updated board"
-    display_board(player_moves, computer_moves)
-    p board_options
-    break if tie?(board_options) || winner?(player_moves, board_options)
-
-    prompt "Computer is O's."  
-    computer_moves << board_options.sample
-    board_options.delete(computer_moves.last)
-    prompt "Computer chose square #{computer_moves.last}"
-
-    prompt "Here's the updated board"
-    display_board(player_moves, computer_moves)
-    p board_options
-    break if tie?(board_options) || winner?(computer_moves, board_options)
+    computer_turn(board)
+    display_board(board)
+    break if tie?(board) || winner?(board[:computer_moves])
   end
-  
-  outcome = case 
-            when winner?(player_moves, board_options)
+
+  outcome = if winner?(board[:player_moves])
               "Nice work, you win!"
-            when winner?(computer_moves, board_options)
+            elsif winner?(board[:computer_moves])
               "Looks like the computer won!"
             else
               "Tie game - nobody won"
             end
   prompt "#{outcome} End round..."
 
+  answer = ''
   loop do
     prompt "Would you like to play again? [Y]es/[N]o"
-    play_again = gets.chomp
-    break unless play_again?(play_again) < 0
-    prompt "#{play_again} is an invalid response"
+    answer = gets.chomp
+    break unless play_again?(answer) < 0
+    prompt "#{answer} is an invalid response"
   end
-  break unless play_again?(play_again) == 1
+  break unless play_again?(answer) == 1
 end
 
 prompt "Thanks for playing!  See you next time :)"
