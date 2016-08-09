6023cdd916f71e2e1ff4d9a966f52efe003b0649
jb2718-SE <jbattle2718@gmail.com>
2016-08-09 10:47:14 -0400

    finished rps v1 and cleared rubocop infractions

--- 120/120_2/oo_rps/rps.rb
+++ 120/120_2/oo_rps/rps.rb
@@ -1,51 +1,93 @@
 class Player
   attr_accessor :name, :move
 
-  def initialize(player_type)
-    # @name = name
-    @player_type = player_type
+  def initialize
+    set_name
+  end
+end
+
+class Human < Player
+  def set_name
+    n = nil
+    loop do
+      puts "What's your name?"
+      n = gets.chomp
+      break unless n.empty?
+      puts "That is not a valid entry.  Try again!"
+    end
+    @name = n
   end
 
   def choose
-    if @player_type == :human
-      choice = nil
-      loop do
-        puts "Choose one: rock, paper, or scissors"
-        choice = gets.chomp.downcase
+    choice = nil
+    loop do
+      puts "Choose one: rock, paper, or scissors"
+      choice = gets.chomp.downcase
 
-        if %w(rock paper scissors).include?(choice)
-          @move = choice
-          puts "You chose: #{@move}"
-          break
-        else
-          puts "That is not a valid choice."
-        end
+      if Move::VALUES.include?(choice)
+        @move = Move.new(choice)
+        puts "#{@name} chose: #{@move}"
+        break
+      else
+        puts "That is not a valid choice."
       end
-    else
-      @move = %w(rock paper scissors).sample
-      puts "Computer chose: #{@move}"
     end
   end
 end
 
-class Move
-  def initialize
+class Computer < Player
+  def set_name
+    @name = ['Number 5', 'R2D2', 'Eve', 'Wall-E'].sample
   end
 
-  def compare
+  def choose
+    @move = Move.new(Move::VALUES.sample)
+    puts "#{@name} chose: #{@move}"
   end
 end
 
-class Rule
-  def initialize
+class Move
+  VALUES = %w(rock paper scissors).freeze
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def rock?
+    @value == 'rock'
+  end
+
+  def paper?
+    @value == 'paper'
+  end
+
+  def scissors?
+    @value == 'scissors'
+  end
+
+  def >(other_move)
+    (rock? && other_move.scissors?) ||
+      (paper? && other_move.rock?) ||
+      (scissors? && other_move.paper?)
+  end
+
+  def <(other_move)
+    (rock? && other_move.paper?) ||
+      (paper? && other_move.scissors?) ||
+      (scissors? && other_move.rock?)
+  end
+
+  def to_s
+    @value
   end
 end
 
 class Game
   attr_accessor :user, :computer
   def initialize
-    @user = Player.new(:human)
-    @computer = Player.new(:computer)
+    @user = Human.new
+    @computer = Computer.new
   end
 
   def display_welcome_message
@@ -56,12 +98,36 @@ class Game
     puts "Thanks for playing Rock, Paper, Scissors.  Good bye!"
   end
 
+  def display_winner
+    if user.move > computer.move
+      puts "#{user.name} won!"
+    elsif user.move < computer.move
+      puts "#{computer.name} won!"
+    else
+      puts "Tie game!"
+    end
+  end
+
+  def play_again?
+    answer = nil
+    loop do
+      puts "Would you like to play again? (y/n)"
+      answer = gets.chomp.downcase
+      break if %w(y n).include?(answer)
+      puts "That is not a valid answer.  Please try again."
+    end
+    return true if answer == 'y'
+    return false if answer == 'n'
+  end
+
   def play
     display_welcome_message
-    user.choose
-    computer.choose
-    # determine_winner
-    # display_winner
+    loop do
+      user.choose
+      computer.choose
+      display_winner
+      break unless play_again?
+    end
     display_goodbye_message
   end
 end
