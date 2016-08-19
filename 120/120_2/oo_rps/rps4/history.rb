3f4b3b460f4e60a647a0ea83684dd1bc9bd52786
jb2718-SE <jbattle2718@gmail.com>
2016-08-19 11:47:22 -0400

    fleshed out methods of AI class

--- 120/120_2/oo_rps/rps4/history.rb
+++ 120/120_2/oo_rps/rps4/history.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class History
   attr_reader :table
 
@@ -23,47 +25,113 @@ class History
   end
 
   def find_by(attribute, value)
-    @table.map do |record|
-      record[attribute] == value
+    @table.select do |record|
+      record[attribute.to_sym] == value
     end
+  end
+
+  def length
+    @table.length
   end
 end
 
 class AI
   
-  def initialize
-    # config settings
+  def initialize(history, enemy, protagonist)
+    @game_hx = history
+    @enemy = enemy
+    @protagonist = protagonist
   end
 
-  def enemy_tendency(move)
-    # If player "tends" to pick <move>
-    # - need to define my window of tendency and the number of tendency
-    # - need a specific move to track
-    #  - formula is something like if the average for a move over the whole history is above a certain threshhold
-    #  - calculate some kind of probabilty and return that
+  def enemy_tendency?(move, threshhold)
+    num_moves = @game_hx.find_by(@enemy, move).length
+    total_moves = @game_hx.length
+    average = num_moves.to_f/total_moves.to_f
+    return true if threshhold < average
+    false
   end
 
-  def enemy_in_a_row(player, move, times)
-      # If opponent has picked <move> <number> of times in a row in most recent history
-      # - need to determine what 'times in a row' is
-      # - calculate some kind of factor/probability that he will do it again & return that
+  def enemy_next_likely?(move, times, window)
+      range_start = (@game_hx.length - window) + 1
+      range_end = @game_hx.length
+      last_n_moves = []
+      (range_start..range_end).each do |idx|
+        last_n_moves << @game_hx.find_by_round(idx - 1)
+      end
+      last_n_moves_count = last_n_moves.length
 
-      # if enemy history includes move x y times in a row
+      counter = 0
+      in_a_rows = []
+      last_n_moves.each do |record|
+        if record[@enemy.to_sym] == move
+          counter +=1 
+        else
+          in_a_rows << counter
+          counter = 0
+        end
+        in_a_rows << counter
+      end
+
+      return true if in_a_rows.max > times
+      false
+
+      # if enemy history includes move <x> y times in a row
       #   pick something that beats x
       # else
       #   don't use this to determine your move
       # end
   end
 
-  def my_weakness(move)
-    # How often do I  get beaten by picking <move>
-    # - return a percentage
-    # - of times I have picked <move>
-    # - of those times, number of times I've been beaten
-    # - I get beaten x percent of the time I pick y:  #times beaten / # times picked (out of total hx)
-    # - this function returns a percentage...it's up to personality to say if percentage is high enough
+  def percent_loss(move)
+    all_moves = @game_hx.find_by(@protagonist, move)
+    all_moves_count = all_moves.length
+    loser_moves = []
+    loser_moves =  all_moves.select do |record|
+                    record[:winner] == @enemy
+                  end
+    times_beaten = loser_moves.length
+    percentage = times_beaten.to_f / all_moves_count.to_f
+    percentage
+  end
+
+  def opposing_move(move)
+    case move
+    when 'rock', 'spock'
+      'paper'
+    when 'paper', 'lizard'
+      'scissors'
+    when 'scissors', 'lizard'
+      'rock'
+    when 'scissors', 'rock'
+      'spock'
+    when 'spock', 'paper'
+      'lizard'
+    end
   end
 
   # part of the personality of different cpu players will be to define the percentages and 
   # threshholds - so each personality will get its own AI object and set the config settings
 end
+
+hx = History.new
+
+r1 = {"user": "rock", "computer": "scissors", "winner": "user"}
+r2 = {"user": "lizard", "computer": "rock", "winner": "computer"}
+r3 = {"user": "scissors", "computer": "rock", "winner": "computer"}
+r4 = {"user": "lizard", "computer": "rock", "winner": "computer"}
+
+hx.insert(r1)
+hx.insert(r2)
+hx.insert(r3)
+hx.insert(r4)
+
+user_ai = AI.new(hx, "computer", "user")
+p user_ai.enemy_tendency?("rock", 0.5)
+
+puts "-------------------------------"
+
+p user_ai.percent_loss("lizard")
+
+puts "-------------------------------"
+
+user_ai.enemy_next_likely?("rock", 2, 3)
