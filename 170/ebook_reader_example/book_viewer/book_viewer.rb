da04592b3616ac61ea2ab513ad7f741be962a25a
jb2718 <vimolabs@gmail.com>
2016-11-16 22:28:31 +0000

    completed hooking up ebook-reader routes and views

--- 170/ebook_reader_example/book_viewer/book_viewer.rb
+++ 170/ebook_reader_example/book_viewer/book_viewer.rb
@@ -2,5 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 
 get "/" do
-  File.read "public/template.html"
+  @title = "The Adventures of Sherlock Homes"
+  @toc_data = File.read("data/toc.txt").split("\n")
+  erb :home
+end
+
+
+get "/chapters/:number" do
+  @num = params[:number].to_i
+  @title = "Chapter " + @num.to_s
+  @toc_data = File.read("data/toc.txt").split("\n")
+  @chapter_data = File.read("data/chp" + @num.to_s + ".txt")
+  erb :chapter
 end
