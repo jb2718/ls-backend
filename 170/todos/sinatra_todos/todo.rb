a7daf2f1654f238284ed3d566aa32fc30aad5081
jb2718 <jbattle2718@gmail.com>
2016-12-13 06:13:00 -0500

    added functionality to display single todo list

--- 170/todos/sinatra_todos/todo.rb
+++ 170/todos/sinatra_todos/todo.rb
@@ -3,48 +3,55 @@ require "sinatra/reloader"
 require "tilt/erubis"
 
 configure do
-	enable :sessions
-	set :session_secret, 'secret'
+  enable :sessions
+  set :session_secret, 'secret'
 end
 
 before do
-	session[:lists] ||= []
+  session[:lists] ||= []
 end
 
 get "/" do
-	redirect "/lists"
+  redirect "/lists"
 end
 
 # View list of lists
 get "/lists" do
-	@lists = session[:lists]
+  @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
 # Render the new list form
 get "/lists/new" do
-	erb :new_list, layout: :layout
+  erb :new_list, layout: :layout
 end
 
 def error_for_list_name(name)
-	if !(1..100).cover?(name.size)
-		"List name must be between 1 and 100 characters"
-	elsif session[:lists].any? {|list| list[:name] == name }
-		"List name must be unique"
-	end	
+  if !(1..100).cover?(name.size)
+    "List name must be between 1 and 100 characters"
+  elsif session[:lists].any? { |list| list[:name] == name }
+    "List name must be unique"
+  end
 end
 
 # Create a new list
 post "/lists" do
-	list_name = params[:list_name].strip
+  list_name = params[:list_name].strip
 
-	error = error_for_list_name(list_name)
-	if error
-		session[:error] = error
-		erb :new_list, layout: :layout
-	else
-		session[:lists] << {name: list_name, todos: []}
-		session[:success] = "The list has been created."
-		redirect "/lists"
-	end
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :new_list, layout: :layout
+  else
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  end
+end
+
+get "/lists/:id" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+
+  erb :list, layout: :layout
 end
